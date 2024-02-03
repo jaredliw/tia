f47ba8c88e05f2cf7092eefa1fc96c8de133111f
Jared Liw Zhi Long <jaredliw@gmail.com>
2024-02-03 21:55:49 +0800

    Init: npm, hugo and sass
    Signed-off-by: wolfishLamb <jaredliw@gmail.com>

---
+++ postscss.config.js
@@ -0,0 +1,11 @@
+const autoprefixer = require('autoprefixer');
+const purgecss = require('@fullhuman/postcss-purgecss');
+
+module.exports = {
+    plugins: [
+        autoprefixer(),
+        purgecss({
+            content: ['./layouts/**/*.html']
+        })
+    ]
+};
